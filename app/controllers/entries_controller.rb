946eacd13f82c1b96f9f1fe7c860157a9d2a37ab
Vincent Gutierrez <vincentgg89@gmail.com>
2016-11-13 07:48:33 -0600

    Implement absences action

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -4,9 +4,14 @@ class EntriesController < ApplicationController
   def search
     current_date = DateTime.now
     @badge_number = params[:badge_number]
-    @period_entries = (1..Time.days_in_month(current_date.month, current_date.year)).map do |day|
-      date = DateTime.new(current_date.year, current_date.month, day)
-      { day: day, record: Entry.by_badge_number(@badge_number).by_day(date).first }
+    @period_entries = get_period_entries(@badge_number)
+  end
+
+  def absences
+    current_date = DateTime.now
+    badges = Entry.select(:badge_number).distinct.actual_period
+    @absences = badges.map do |badge|
+      { badge_number: badge.badge_number, period_entries: get_period_entries(badge.badge_number) }
     end
   end
 
@@ -15,5 +20,13 @@ class EntriesController < ApplicationController
     current_date = DateTime.now
     days_before_paycheck = current_date.end_of_month.mday - current_date.mday
     redirect_to root_path, notice: 'Your report will be available 3 days before the payday' if days_before_paycheck > 3
+  end
+
+  def get_period_entries(badge_number)
+    current_date = DateTime.now
+    (1..Time.days_in_month(current_date.month, current_date.year)).map do |day|
+      date = DateTime.new(current_date.year, current_date.month, day)
+      { day: day, record: Entry.by_badge_number(badge_number).by_day(date).first }
+    end
   end
 end
--- app/models/entry.rb
+++ app/models/entry.rb
@@ -4,4 +4,6 @@ class Entry < ApplicationRecord
   scope :not_checked_out, -> { where(check_out: nil) }
   scope :by_badge_number, ->(badge_number) { where(badge_number: badge_number) }
   scope :by_day, ->(entry_date) { where("check_in BETWEEN ? AND ?", entry_date.beginning_of_day, entry_date.end_of_day) }
+  scope :actual_period, -> { where("check_in BETWEEN ? AND ?", DateTime.now.beginning_of_month, DateTime.now.end_of_month)  }
+
 end
