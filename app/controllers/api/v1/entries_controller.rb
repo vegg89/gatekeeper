d1159f76d88e8e61d03b2a3d2ccb83fb258ac169
Vincent Gutierrez <vincentgg89@gmail.com>
2016-11-13 04:52:05 -0600

    Add blank entries controller and model

---
+++ app/controllers/api/v1/entries_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::EntriesController < ApplicationController
+  protect_from_forgery with: :null_session
+
+  def in
+    # TODO: Implemente In action
+  end
+
+  def out
+    # TODO: Implement Out action
+  end
+end
