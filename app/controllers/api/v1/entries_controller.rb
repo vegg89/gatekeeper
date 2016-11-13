3957b92a61e2f610fb864ff7b83a606625ec3f16
Vincent Gutierrez <vincentgg89@gmail.com>
2016-11-13 04:57:22 -0600

    Implement In action for entries controller and add some useful scopes

--- app/controllers/api/v1/entries_controller.rb
+++ app/controllers/api/v1/entries_controller.rb
@@ -2,7 +2,17 @@ class Api::V1::EntriesController < ApplicationController
   protect_from_forgery with: :null_session
 
   def in
-    # TODO: Implemente In action
+    entry = Entry.by_badge_number(params[:badge_number])
+      .by_day(DateTime.parse(params[:datetime])).first
+    if entry
+      render json: "", status: 304
+    else
+      entry = Entry.new()
+      entry.badge_number = params[:badge_number]
+      entry.check_in = params[:datetime]
+      entry.save
+      render json: entry, status: 201
+    end
   end
 
   def out
