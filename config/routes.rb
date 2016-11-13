24871607388820dacfae54b9a6c0e9dd4c867cf3
Vincent Gutierrez <vincentgg89@gmail.com>
2016-11-13 05:20:11 -0600

    Add a simple home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#home'
+  
   namespace :api do
     namespace :v1 do
       post '/entries/:badge_number/in', to: 'entries#in'
       post '/entries/:badge_number/out', to: 'entries#out'
     end
   end
+
+  get 'static_pages/home', to: 'static_pages#home'
 end
